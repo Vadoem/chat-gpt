adb739ed6f30be6f220f52b69784df2a7b731e0f
Eduard <eduardkarimov.rb@gmail.com>
2023-05-31 12:50:14 +0500

    new futures

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -1,2 +1,2 @@
 String BASE_URL = "https://api.openai.com/v1";
-String API_KEY = "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";
+String API_KEY = "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";
